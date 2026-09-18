807cb0591d8b23b625df5bfca8e72ac926699c3f
Adam Nagy <joevagyok@gmail.com>
2026-09-18 11:40:47 +0200

    OEL-4958: Send compact JSON to the model in prompts and tool results.

--- src/Plugin/AiEditorialAssistant/DraftingPlugin.php
+++ src/Plugin/AiEditorialAssistant/DraftingPlugin.php
@@ -971,7 +971,7 @@ class DraftingPlugin extends AiAssistantPluginBase {
         $context['entityTypeId'], $context['bundle'], $context['template']
       );
       $prompt .= "\nAvailable field groups:\n"
-        . json_encode($groups, JSON_PRETTY_PRINT) . "\n";
+        . json_encode($groups) . "\n";
     }
 
     return $prompt;
